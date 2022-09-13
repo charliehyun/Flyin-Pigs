2764262a2419a32060934c932c6ed4bc41c7da14
Mark <marklim4@gmail.com>
2022-09-12 22:24:41 -0400

    Initial push of code
    mean stack

---
+++ flyingPigs/client/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'employees', pathMatch: 'full' },
+  { path: 'employees', component: EmployeesListComponent },
+  { path: 'employees/new', component: AddEmployeeComponent },
+  { path: 'employees/edit/:id', component: EditEmployeeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
