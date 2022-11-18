0b05bd5344984a543c96d119c762b76cd61f65cb
Jessica Yeh <jessicayeh@Jessicas-Air.local>
2022-11-18 11:56:52 -0500

    feedback page

--- flyingPigs/client/src/app/feedback/feedback.component.ts
+++ flyingPigs/client/src/app/feedback/feedback.component.ts
@@ -8,7 +8,8 @@ import { Router } from "@angular/router";
   })
   
   export class FeedbackComponent {
-    
+    email: string;
+    comments: string;
   
     constructor( private router: Router) {
   
